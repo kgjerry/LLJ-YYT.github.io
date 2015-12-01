40553baedbf18d95e2f8035071d157c8e377f4b8
kgjerry <jerry2003tsai@hotmail.com>
2015-12-01 14:47:31 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,13 @@
 
+var userChoice = prompt ("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+console.log(computerChoice);
+
+if (0<computerChoice<0.33)
+    computerChoice = "rock";
+
+else if (0.34<computerChoice<0.66)
+    computerChoice = "paper";
+
+else (0.67<computerChoice<1)
+    computerChoice = "scissors";
